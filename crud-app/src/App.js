df9724295bdfb3ec84193bf49c099993f0f745db
Mrudul-Panchal <mrudul@webcluesinfotech.com>
2021-08-16 17:41:13 +0530

    7356a6e9028c32368008a33f4d3c79d0da219c2a

---
+++ crud-app/src/App.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import {  Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Navbar from "./components/Navbar";
+import Login from './components/Login';
+import Signup from './components/Signup';
+import './App.css';
+import Errorpage from './components/Errorpage';
+import Logout from './components/Logout';
+
+ 
+
+const App = () => {
+    return (
+<>
+<Navbar />
+
+<Switch>
+
+<Route exact path="/">
+<Home /> 
+</Route>
+
+<Route path="/about">
+<About />
+</Route>
+
+<Route path="/contact">
+<Contact />
+</Route>
+
+<Route path="/login">
+<Login />
+</Route>
+
+<Route path="/signup">
+<Signup />
+</Route>
+
+<Route path="/logout">
+ <Logout />
+</Route>
+
+
+
+<Route>
+    
+    <Errorpage />
+        
+    
+</Route>
+
+</Switch>
+
+</>
+
+        )
+}
+
+export default App
